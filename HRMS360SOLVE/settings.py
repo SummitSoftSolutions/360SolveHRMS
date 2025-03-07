13c6eb4f6287e22d73f982b16933bef1a722351d
Midhun <midhuncv728@gmail.com>
2025-03-07 15:20:42 -0500

    once more pull

--- HRMS360SOLVE/settings.py
+++ HRMS360SOLVE/settings.py
@@ -76,16 +76,27 @@ WSGI_APPLICATION = 'HRMS360SOLVE.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 load_dotenv()
+# DATABASES = {
+#     'default': {
+        
+        
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':os.getenv('DB_NAME'),
+#         'USER':os.getenv('DB_USER'),
+#         'PASSWORD':os.getenv('DB_PASSWORD'),
+#         'HOST':os.getenv('DB_HOST'),
+#         'PORT':os.getenv('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        
-        
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':os.getenv('DB_NAME'),
-        'USER':os.getenv('DB_USER'),
-        'PASSWORD':os.getenv('DB_PASSWORD'),
-        'HOST':os.getenv('DB_HOST'),
-        'PORT':os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
