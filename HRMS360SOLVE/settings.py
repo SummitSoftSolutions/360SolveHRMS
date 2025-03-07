83ef9e5f5a3ac56060d1c3f92cc4609a6f62273f
Midhun <midhuncv728@gmail.com>
2025-03-07 17:36:34 -0500

    jwt added in settings

--- HRMS360SOLVE/settings.py
+++ HRMS360SOLVE/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv  import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,32 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'HRMSapp',
+    'AdminApp',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
+
+REST_FRAMEWORK = {
+    
+    "DEFAULT_AUTHENTICATION_CLASSES":(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS':True,
+    'BLACKLIST_AFTER_ROTATION':True,
+    'SIGNING KEY':SECRET_KEY,
+    'ALGORITHM':'HS256',
+    
+    
+}
+
+SIMPLE_JWT['BLACKLIST_AFTER_ROTATION'] = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,19 +101,11 @@ WSGI_APPLICATION = 'HRMS360SOLVE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+from dotenv import load_dotenv
+
+# Load .env file
 load_dotenv()
-# DATABASES = {
-#     'default': {
-        
-        
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':os.getenv('DB_NAME'),
-#         'USER':os.getenv('DB_USER'),
-#         'PASSWORD':os.getenv('DB_PASSWORD'),
-#         'HOST':os.getenv('DB_HOST'),
-#         'PORT':os.getenv('DB_PORT'),
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -99,6 +117,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
