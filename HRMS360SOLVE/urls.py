d822778fda8bbd08613dac08b6676104b89b3273
Priyanka <priyankataug98@gmail.com>
2025-03-11 12:21:21 +0530

    Added superadmin

--- HRMS360SOLVE/urls.py
+++ HRMS360SOLVE/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='HRMS API',
+        default_version='v1',
+        description="HR Management System API Documentation",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('HRMSapp.urls')),
+    path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='swagger-ui'),
+    path('redoc/',schema_view.with_ui('redoc',cache_timeout=0),name='redoc-ui'),
+      
 ]
--- HRMSapp/views.py
+++ HRMSapp/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets,status
+from .serializers import *
+from  rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# Generating user token
+
+
 
 
 # Create your views here.
+class SuperAdminViewSet(viewsets.ViewSet):
+    permission_classes = [AllowAny]
+    def create(self, request):
+        print("Received OTP verification request!")  
+        email = request.data.get("email")
+        password = request.data.get("password")
+        try:
+            user = SuperAdmin.objects.get(email=email,Password=password)
+            print(f"User found: {user.email}")  
+
+            # Generate JWT tokens
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+ 
+            print(f"Tokens generated: access={access_token}, refresh={refresh_token}")  
+ 
+            return Response({
+                "status": "success",
+                "message": "OTP verified successfully!",
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }, status=status.HTTP_200_OK)
+ 
+        except SuperAdmin.DoesNotExist:
+            print("User not found!")  
+            return Response({"status": "error", "message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
