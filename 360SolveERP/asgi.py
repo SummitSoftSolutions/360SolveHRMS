99b666a48c7d73ff48ef504e3a83de0057430e76
Midhun <midhuncv728@gmail.com>
2025-03-19 15:31:42 -0400

    docker added

--- 360SolveERP/asgi.py
+++ 360SolveERP/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRMS360SOLVE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '360SolveERP.settings')
 
 application = get_asgi_application()
